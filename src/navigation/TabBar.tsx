7dba84c2e7da7b47ae67d190bde6b5b204e97ce9
Büşra Arıgün <busrarigun@gmail.com>
2023-09-13 00:59:09 +0300

    Tabbar created

---
+++ src/navigation/TabBar.tsx
@@ -0,0 +1,51 @@
+import {View, Text, TouchableOpacity} from 'react-native';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+// import {Colors} from '../src/constants/Colors';
+export function TabBar({state, descriptors, navigation}: any) {
+  return (
+    <View style={{flexDirection: 'row'}}>
+      {state.routes.map((route: any, index: any) => {
+        const {options} = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+        const isFocused = state.index === index;
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+          if (!isFocused && !event.defaultPrevented) {
+            // The `merge: true` option makes sure that the params inside the tab screen are preserved
+            navigation.navigate({name: route.name, merge: true});
+          }
+        };
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? {selected: true} : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{flex: 1}}>
+              <View style={{borderWidth:1}}/>
+            {/* <View style={{alignSelf:'center',marginVertical:30}}>
+              <Icon name={options.tabBarIcon} size={30}  />
+            </View> */}
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
