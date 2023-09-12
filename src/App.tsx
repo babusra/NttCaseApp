427409b03d2c1a448c1a5ab35f0dcac170fc2710
Büşra Arıgün <busrarigun@gmail.com>
2023-09-13 00:59:51 +0300

    App.tsx revised

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
+import {Provider} from 'react-redux';
+import {Store} from './reduxToolkit/store';
+import Navigation from './navigation/Navigation';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <React.Fragment>
+      <Provider store={Store}>
+        <Navigation />
+      </Provider>
+    </React.Fragment>
+  );
+};
 
-export default App
+export default App;
