b44846c01506d15f1f52a3e346ed9ccb8a71afd7
Büşra Arıgün <busrarigun@gmail.com>
2023-09-14 00:03:04 +0300

    Screens file created

--- src/navigation/Screens.ts
+++ src/navigation/Screens.ts
@@ -1,4 +1,8 @@
 export const Screens = {
   Login: 'LoginScreen',
-  Home:'HomeScreen'
+  Home: 'HomeScreen',
+  Favorites: 'FavoritesScreen',
+  Basket: 'BasketScreen',
+  ProductDetail: 'ProductDetailScreen',
+  ProductList: 'ProductListScreen',
 };
