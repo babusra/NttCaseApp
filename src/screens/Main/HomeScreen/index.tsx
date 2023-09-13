27b69de690e6e03a55fd334052d62313d8c83040
Büşra Arıgün <busrarigun@gmail.com>
2023-09-14 00:07:52 +0300

    HomeScreen initialized

---
+++ src/screens/Main/HomeScreen/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React, {FC} from 'react';
+
+const HomeScreen: FC = () => {
+  return (
+    <View>
+      <Text>:dfgfgdfg</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
