205e1f4e4fc88760a54e56b8614913d9e881a264
Büşra Arıgün <busrarigun@gmail.com>
2023-09-14 00:08:06 +0300

    ProductDetailScreen initialized

---
+++ src/screens/Main/ProductDetailScreen/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React, {FC} from 'react';
+
+const ProductDetailScreen: FC = () => {
+  return (
+    <View>
+      <Text>ProductDetailScreen</Text>
+    </View>
+  );
+};
+
+export default ProductDetailScreen;
