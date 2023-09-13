7d03e5df519150cc403989d7881a6a9a93feeb19
Büşra Arıgün <busrarigun@gmail.com>
2023-09-14 00:07:39 +0300

    Favorites Screen initialized

---
+++ src/screens/Main/FavoritesScreen/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React, {FC} from 'react';
+
+const FavoritesScreen: FC = () => {
+  return (
+    <View>
+      <Text>FavoritesScreen</Text>
+    </View>
+  );
+};
+
+export default FavoritesScreen;
